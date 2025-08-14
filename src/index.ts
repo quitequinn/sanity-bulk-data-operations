0e6f34e49214c28498d13b9a0d3be06e3922efcc
Quinn Keaveney <quinn@liiift.studio>
2025-08-14 11:07:42 -0700

    Update index to export BulkDataOperations component

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Sanity Bulk Data Operations - Main Export
+ * Flexible bulk data operations utility for Sanity Studio
+ */
+
+export { default as BulkDataOperations } from './BulkDataOperations'
+export { default } from './BulkDataOperations'
+
+// Export types if needed
+export type {
+  BulkDataOperationsProps,
+  BulkOperationResult
+} from './BulkDataOperations'
